cf1278a6b1f44473285a5071346dd5c1a4dbc4f6
Bruno Valle <brunolv6@gmail.com>
2021-07-15 19:25:17 -0300

    filtro pelo query para recebimento de dados e pegar registros específicos no bd

--- src/main/kotlin/br/com/zup/autores/BuscaAutoresController.kt
+++ src/main/kotlin/br/com/zup/autores/BuscaAutoresController.kt
@@ -3,18 +3,34 @@ package br.com.zup.autores
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 
 
 @Controller("/autores")
 class BuscaAutoresController(val autorRepository: AutorRepository) {
 
+    // opcional autores/?email=qualquer@gmail.com
+    // como coloquei defaultValue vazio, então não é obrigatório
     @Get
-    fun lista() : HttpResponse<List<DetalhesDosAutoresResponse>>{
-        val autores: List<Autor> = autorRepository.findAll()
+    fun lista(@QueryValue(defaultValue = "") email: String) : HttpResponse<Any>{
 
-        val response = autores.map{ autor -> DetalhesDosAutoresResponse(autor) }
+        if(email.isEmpty()){
+            val autores: List<Autor>  = autorRepository.findAll()
 
-        return HttpResponse.ok(response)
+            val response = autores.map{ autor -> DetalhesDosAutoresResponse(autor) }
+
+            return HttpResponse.ok(response)
+        }
+
+        val possivelAutor = autorRepository.findByEmail(email)
+
+        if(possivelAutor.isEmpty) {
+            return HttpResponse.notFound()
+        }
+
+        val autor = possivelAutor.get()
+
+        return HttpResponse.ok(DetalhesDosAutoresResponse(autor))
 
     }
 }
